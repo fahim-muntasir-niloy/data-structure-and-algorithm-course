c98f40aee3015c65f276d2b79e19c68f8abaf1dc
Fahim Muntasir <niloyf3@gmail.com>
2023-06-09 12:19:38 +0600

    32/38 passed

--- Array/anagram.py
+++ Array/anagram.py
@@ -1,13 +1,23 @@
 def isAnagram(s,t):
-    dup_set = set()
+    s_set = set()
+    whole_set = set()
+    
     if s == t:
         return True
+    elif t<s:
+        return False
+    else:
+        for i in s:
+            s_set.add(i)
+            whole_set.add(i)
+            
+        for i in t:
+            whole_set.update(i)
+
+        
+        return s_set, whole_set, len(s_set)==len(whole_set)
 
             
         
     
-print(isAnagram("rat","rat"))
-
-word = "anagram"
-letters = [i for i in word]
-print(letters)
+print(isAnagram("aacc","ccac"))
