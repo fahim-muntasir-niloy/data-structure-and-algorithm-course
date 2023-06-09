1da92f3952f37e8e1c025638477889975bf4b0fa
Fahim Muntasir <niloyf3@gmail.com>
2023-06-09 10:16:13 +0600

    duplicate finalized with function scope variable

---
+++ Array/anagram.py
@@ -0,0 +1,13 @@
+def isAnagram(s,t):
+    dup_set = set()
+    if s == t:
+        return True
+
+            
+        
+    
+print(isAnagram("rat","rat"))
+
+word = "anagram"
+letters = [i for i in word]
+print(letters)
--- Array/duplicate.py
+++ Array/duplicate.py
@@ -1,11 +1,12 @@
 # a = [21,1,2,2,2,2,2,2,2] 
 a = [2,8,22,1,4,22]
-hash_set = set()
+
 
 # This also works taking hash_set as empty list and appending items. But it is slower than adding to set.
 
 
 def duplicate(arr):
+   hash_set = set()     # Variable should be function scope rather than global scope.
    for j in arr:
       if j in hash_set:
          return True
@@ -15,5 +16,4 @@ def duplicate(arr):
 
 print(duplicate(a))
 print(len(a))
-print(len(hash_set))
 
