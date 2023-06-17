db443e392f3b00e5521fc6709a4a374bc159f9b4
Data-poeple-codes <datapeople34@gmail.com>
2023-06-17 19:03:01 +0600

    two sum added

---
+++ Array/hashmap.md
@@ -0,0 +1,3 @@
+# Hashmap in Python
+
+[Hash_Maps](https://www.youtube.com/watch?v=RcZsTI5h0kg)
--- Array/two_sum.py
+++ Array/two_sum.py
@@ -1,14 +1,26 @@
-arr = [3,4,6,2]   
-n = 6
+arr = [3,4,7,1,2]   
+n = 9
 
-# brute force
+# bad brute force
 
-for i in arr:
-    for j in arr:
-        if(i+j==n):
-            if(i==j):
-                pass
-            else:
-                print([i,j])
+# for i in arr:
+#     for j in arr:
+#         if(i+j==n):
+#             if(i==j):
+#                 pass
+#             else:
+#                 print([i,j])
 
-# This is a bad solution with complexity O(n^2); Adding more steps would make it more inefficient.
+# This is a bad solution with complexity O(n^2); 
+# Adding more steps would make it more inefficient.
+
+# plus minus
+def twosum(arr, n):
+    hash_map = {}       # value : Index
+    for i,j in enumerate(arr):
+        diff = n-j
+        if diff in hash_map:
+            return (hash_map[diff], i)
+        hash_map[j] = i
+
+print(twosum(arr,n))
